81086025f8ab76642ead02e22af9863aaad1d034
Daniel Wire <122326404+livewiredeveloper@users.noreply.github.com>
2023-07-12 21:25:27 -0700

    Update home.js
    added helper function, defined "getBorrowedCount"

--- public/src/home.js
+++ public/src/home.js
@@ -1,11 +1,10 @@
 function getTotalBooksCount(books) {
-  return (books.length);
+  return books.length;
 }
 
 function getTotalAccountsCount(accounts) {
-  return (accounts.length);
+  return accounts.length;
 }
-
 //helper function
 function getBooksBorrowedCount(books) {
   let count = 0;
@@ -14,49 +13,83 @@ function getBooksBorrowedCount(books) {
   });
   return count;
 }
+function getBorrowedCount(book, count) {
+  book.borrows.forEach((borrow) => {
+    if (!borrow.returned) count = count + 1;
+  });
+  return count;
+}
+
+//helper funtion
+function _sortObjectByValues(obj) {
+  const keys = Object.keys(obj);
+  return keys.sort((keyA, keyB) => {
+    if (obj[keyA] > obj[keyB]) {
+      return -1;
+    } else if (obj[keyB] > obj[keyA]) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+}
 
 function getMostCommonGenres(books) {
-  let genresCount = {};
-  books.forEach(book => {
-    const { genre } = book;
-    genresCount[genre] = (genresCount[genre] || 0) + 1;
-  })
-  const genres = Object.entries(genresCount).map(([name, count]) => ({ name, count }));
-  genres.sort((a, b) => b.count - a.count);
+  const count = books.reduce((acc, { genre }) => {
+    if (acc[genre]) {
+      acc[genre] += 1;
+    } else {
+      acc[genre] = 1;
+    }
 
-  return genres.slice(0, 5);
+    return acc;
+  }, {});
+
+  const sorted = _sortObjectByValues(count);
+  return sorted.map((name) => ({ name, count: count[name] })).slice(0, 5);
 }
 
 function getMostPopularBooks(books) {
-  const popularBooks = [];
+  const groupById = books.reduce((acc, { id, borrows }) => {
+    acc[id] = borrows.length;
+    return acc;
+  }, {});
 
-  const sortedBooks = books.sort((a, b) => b.borrows.length - a.borrows.length);
-
-  for (let i = 0; i < Math.min(sortedBooks.length, 5); i++) {
-    const book = sortedBooks[i];
-    popularBooks.push({ name: book.title, count: book.borrows.length });
-  }
-
-  return popularBooks;
+  const sorted = _sortObjectByValues(groupById);
+  return sorted
+    .map((id) => {
+      const { title: name } = books.find(({ id: bookId }) => bookId === id);
+      return { name, count: groupById[id] };
+    })
+    .slice(0, 5);
 }
 
 function getMostPopularAuthors(books, authors) {
-  const authorCounts = {};
-
-  for (const book of books) {
-    const author = authors.find((author) => author.id === book.authorId);
-    if (author) {
-      const { name: { first, last } } = author;
-      const authorName = `${first} ${last}`;
-      authorCounts[authorName] = (authorCounts[authorName] || 0) + book.borrows.length;
+  const count = books.reduce((acc, { authorId, borrows }) => {
+    if (acc[authorId]) {
+      acc[authorId].push(borrows.length);
+    } else {
+      acc[authorId] = [borrows.length];
     }
+
+    return acc;
+  }, {});
+
+  for (let id in count) {
+    const sum = count[id].reduce((a, b) => a + b);
+    count[id] = sum;
   }
 
-  const popularAuthors = Object.entries(authorCounts).map(([name, count]) => ({ name, count }));
-
-  popularAuthors.sort((a, b) => b.count - a.count);
-
-  return popularAuthors.slice(0, 5);
+  const sorted = _sortObjectByValues(count);
+  return sorted
+    .map((authorId) => {
+      const {
+        name: { first, last },
+      } = authors.find(({ id }) => id === Number(authorId));
+      const name = `${first} ${last}`;
+      return { name, count: count[authorId] };
+    })
+    .slice(0, 5);
 }
 
 module.exports = {
